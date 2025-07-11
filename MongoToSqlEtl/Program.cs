9c9bfaf100ebfc3e367bfe8e615e765882c0ba42
llthanh714@live.com <llthanh714@live.com>
2025-07-11 12:12:11 +0700

    Cấu hình thêm một số tính năng nâng cao của Hangfire

--- MongoToSqlEtl/Program.cs
+++ MongoToSqlEtl/Program.cs
@@ -2,6 +2,8 @@
 using ETLBox.SqlServer;
 using Hangfire;
 using Hangfire.Console;
+using Hangfire.Dashboard;
+using Hangfire.Dashboard.BasicAuthorization;
 using Hangfire.SqlServer;
 using MongoDB.Driver;
 using MongoToSqlEtl.Jobs;
@@ -23,6 +25,16 @@ try
 
     var builder = WebApplication.CreateBuilder(args);
 
+    // Kiểm tra và lấy mật khẩu từ biến môi trường
+    var sqlPassword = Environment.GetEnvironmentVariable("__DB_PASSWORD__", EnvironmentVariableTarget.Machine);
+    var mongoPassword = Environment.GetEnvironmentVariable("__MONGOGDB_PASSWORD__", EnvironmentVariableTarget.Machine);
+
+    if (string.IsNullOrWhiteSpace(sqlPassword))
+        throw new Exception("__DB_PASSWORD__ environment variable is not set.");
+
+    if (string.IsNullOrWhiteSpace(mongoPassword))
+        throw new Exception("__MONGOGDB_PASSWORD__ environment variable is not set.");
+
     // --- STEP 2: Tích hợp Serilog vào ASP.NET Core ---
     builder.Host.UseSerilog();
 
@@ -35,14 +47,14 @@ try
     {
         var config = sp.GetRequiredService<IConfiguration>();
         string? cs = config.GetConnectionString("SqlServer") ?? throw new InvalidOperationException("Connection string 'SqlServer' not found.");
-        return new SqlConnectionManager(cs);
+        return new SqlConnectionManager(cs.Replace("__DB_PASSWORD__", sqlPassword));
     });
 
     builder.Services.AddSingleton(sp =>
     {
         var config = sp.GetRequiredService<IConfiguration>();
         string? cs = config.GetConnectionString("MongoDb") ?? throw new InvalidOperationException("Connection string 'MongoDb' not found.");
-        return new MongoClient(cs);
+        return new MongoClient(cs.Replace("__MONGOGDB_PASSWORD__", mongoPassword));
     });
 
     // Đăng ký các dịch vụ khác
@@ -57,12 +69,17 @@ try
     builder.Services.AddTransient<PatientOrdersEtlJob>();
 
     // --- STEP 4: Cấu hình Hangfire ---
+    var hangfireConfig = builder.Configuration.GetConnectionString("Hangfire");
+    if (string.IsNullOrWhiteSpace(hangfireConfig))
+        throw new InvalidOperationException("Connection string 'Hangfire' not found.");
+
     builder.Services.AddHangfire(configuration => configuration
         .SetDataCompatibilityLevel(CompatibilityLevel.Version_180)
         .UseSimpleAssemblyNameTypeSerializer()
         .UseRecommendedSerializerSettings()
-        .UseSqlServerStorage(builder.Configuration.GetConnectionString("HangfireConnection"), new SqlServerStorageOptions
+        .UseSqlServerStorage(hangfireConfig.Replace("__DB_PASSWORD__", sqlPassword), new SqlServerStorageOptions
         {
+            JobExpirationCheckInterval = TimeSpan.FromDays(15),
             CommandBatchMaxTimeout = TimeSpan.FromMinutes(5),
             SlidingInvisibilityTimeout = TimeSpan.FromMinutes(5),
             QueuePollInterval = TimeSpan.Zero,
@@ -79,7 +96,28 @@ try
 
     // --- STEP 6: Cấu hình Request Pipeline và Dashboard ---
     app.UseSerilogRequestLogging();
-    app.UseHangfireDashboard("/hangfire"); // Kích hoạt Dashboard tại /hangfire
+
+    var hangfireOptions = new DashboardOptions
+    {
+        Authorization =
+        [
+            new BasicAuthAuthorizationFilter(new BasicAuthAuthorizationFilterOptions
+            {
+                RequireSsl = true,
+                LoginCaseSensitive = true,
+                Users =
+                [
+                    new BasicAuthAuthorizationUser
+                    {
+                        Login = "admin",
+                        PasswordClear = "123567899"
+                    }
+                ]
+            })
+        ]
+    };
+
+    app.UseHangfireDashboard("/hangfire", hangfireOptions);
 
     // --- STEP 7: Đăng ký các Job định kỳ (Recurring Jobs) ---
     // Job này sẽ được tự động thêm/cập nhật khi ứng dụng khởi động
